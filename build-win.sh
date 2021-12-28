c2a320262f8d870d0819afc0d826187d38b0b209
Abheek Dhawan <abheekd@protonmail.com>
2021-12-28 14:11:40 -0600

    Fix Windows build file name

--- build-win.sh
+++ build-win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use MINGW for cross-compilation
-x86_64-w64-mingw32-g++-win32 main.cpp modlimitfix.res -std=c++17
+x86_64-w64-mingw32-g++-win32 main.cpp modlimitfix.res -o modlimitfix.exe -std=c++17
