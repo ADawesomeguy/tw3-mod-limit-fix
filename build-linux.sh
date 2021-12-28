501a2cfcbbb1d9a37fa2d778d17b15b206eac759
Abheek Dhawan <abheekd@protonmail.com>
2021-12-28 13:11:37 -0600

    Add simple build bash scripts

---
+++ build-linux.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ main.cpp -o modlimitfix --std=c++17
---
+++ build-win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Use MINGW for cross-compilation
+x86_64-w64-mingw32-g++-win32 main.cpp modlimitfix.res -std=c++17
