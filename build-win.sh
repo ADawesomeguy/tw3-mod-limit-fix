ed9991aa11fa445382bf2c740cd8a56e74665ac2
Abheek Dhawan <abheekd@protonmail.com>
2021-12-28 20:55:48 -0600

    Don't add icon to Windows build
    For some reason adding an icon causes the EXE to be flagged by Nexus as
    suspicious even though it passes the VirusTotal test

--- build-win.sh
+++ build-win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use MINGW for cross-compilation
-x86_64-w64-mingw32-g++ main.cpp modlimitfix.res -o modlimitfix.exe -std=c++17
+x86_64-w64-mingw32-g++ main.cpp -o modlimitfix.exe -std=c++17
